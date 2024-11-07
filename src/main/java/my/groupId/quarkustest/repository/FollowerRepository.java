e5e49bc0d83c4cd536113e0a879bf5bfaa92f60a
joaobncarvalho <joaobernardocarvalho02@gmail.com>
2024-11-07 13:06:58 +0000

    Alterações - Implementação de Repositories, Requests e Entities.

--- src/main/java/my/groupId/quarkustest/repository/FollowerRepository.java
+++ src/main/java/my/groupId/quarkustest/repository/FollowerRepository.java
@@ -1,5 +1,5 @@
 package my.groupId.quarkustest.repository;
-
+//comment
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import my.groupId.quarkustest.domain.user.Follower;
